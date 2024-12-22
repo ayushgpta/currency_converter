48e5c6b6b89175a6c88702084a569e998a644d2d
ayushgpta <ayushdoescoding@gmail.com>
2024-12-22 12:46:20 +0530

    installed tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './index.css'
+
+
+function App() {
+ 
+
+  return (
+   <>
+   <h1 className='text-3xl bg-orange-500'>heading</h1>
+   </>
+  )
+}
+
+export default App;
