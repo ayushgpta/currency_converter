ae3044cb8e8243da40581d0561861c87f2e86226
ayushgpta <ayushdoescoding@gmail.com>
2024-12-22 14:38:32 +0530

    add inputbox component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import './index.css'
 
 
+
 function App() {
  
 
   return (
    <>
-   <h1 className='text-3xl bg-orange-500'>heading</h1>
+   <InputBox label="from"/>
    </>
   )
 }
